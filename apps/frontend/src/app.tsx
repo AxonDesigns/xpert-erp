bed8267ae5d00db8af33c08afbcaba6cbf10272b
AxonDesigns <acciol400@gmail.com>
2025-06-18 20:49:19 -0500

    feat: add react query to router's context

--- apps/frontend/src/app.tsx
+++ apps/frontend/src/app.tsx
@@ -3,9 +3,10 @@ import { RouterProvider, createRouter } from '@tanstack/react-router'
 import { routeTree } from './routeTree.gen'
 import { useAuth } from '@frontend/hooks/useAuth'
 import { Toaster } from './components/ui/sonner'
+import { QueryClient } from '@tanstack/react-query'
 
 // biome-ignore lint/style/noNonNullAssertion: <explanation>
-const router = createRouter({ routeTree, context: { auth: undefined! } })
+const router = createRouter({ routeTree, context: { auth: undefined!, queryClient: undefined! } })
 
 declare module '@tanstack/react-router' {
   interface Register {
@@ -13,18 +14,16 @@ declare module '@tanstack/react-router' {
   }
 }
 
+const queryClient = new QueryClient();
 
 const App = () => {
   const auth = useAuth();
-
-  if (auth.isLoading) {
-    return null;
-  }
 
   return (
     <>
       <RouterProvider router={router} context={{
         auth,
+        queryClient
       }} />
       <Toaster />
     </>
